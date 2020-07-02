b346948b4242ae60351eb8794e44ba4371201711
dustine32 <dustine32@gmail.com>
2020-07-01 17:38:31 -0700

    Tests for issue #20

--- test.py
+++ test.py
@@ -23,11 +23,19 @@ class TestSignor2Gocam(unittest.TestCase):
         go_term = mapping_set.go_id_by_mechanism("catalytic activity")
         self.assertEqual(go_term, "GO:0003824")
 
+    @staticmethod
+    def mod_prefix_entity(entity_id):
+        if ":" in entity_id:
+            return entity_id
+        else:
+            return f"UniProtKB:{entity_id}"
+
     def run_query(self, model: GoCamModel, query):
         prefix_context = {
             "RO": "http://purl.obolibrary.org/obo/RO_",
             "UniProtKB": "http://identifiers.org/uniprot/",
-            "GO": "http://purl.obolibrary.org/obo/GO_"
+            "GO": "http://purl.obolibrary.org/obo/GO_",
+            "CHEBI": "http://purl.obolibrary.org/obo/CHEBI_"
         }
         graph = model.writer.writer.graph
         response = graph.query(prepareQuery(query, initNs=prefix_context))
@@ -63,6 +71,23 @@ class TestSignor2Gocam(unittest.TestCase):
                     ?activity {intermediary_relation} ?int_bp .
                     ?int_bp {reg_relation} ?unknown .
                     ?unknown {enabled_by} ?entity_b
+                }}
+                """
+        return query
+
+    @staticmethod
+    def gen_participant_stmt_query(entity_a, mechanism, entity_b, participant_relation):
+        enabled_by = "RO:0002333"
+        entity_a = TestSignor2Gocam.mod_prefix_entity(entity_a)
+        entity_b = TestSignor2Gocam.mod_prefix_entity(entity_b)
+        query = f"""
+                SELECT ?s ?activity
+                WHERE {{ 
+                    ?s rdf:type {entity_a} .
+                    ?activity rdf:type {mechanism} .
+                    ?entity_b rdf:type {entity_b} .
+                    ?activity {enabled_by} ?s .
+                    ?activity {participant_relation} ?entity_b
                 }}
                 """
         return query
@@ -137,20 +162,64 @@ class TestSignor2Gocam(unittest.TestCase):
         # TODO: Now check how many instances of most_prominent_entity are in model. Should this == input count?
         print(most_prominent_entity, entity_counts[most_prominent_entity])
 
+    def test_direct_interaction(self):
+        # P49841 (GSK3B)<-protein kinase activity-directly positively regulates->MF->P17676 (CEBPB)
+        # From SIGNOR-AC
+        stmt_file = "resources/test/SIGNOR-AC.tsv"
+        model = generate_model(stmt_file, "SIGNOR - Adipogenesis")
+
+        resp = self.run_query(model, self.gen_causal_stmt_query(entity_a="P49841",
+                                                                mechanism="GO:0004672",
+                                                                entity_b="P17676",
+                                                                reg_relation=OntologyTerm.DIRECTLY_POSITIVELY_REGULATES.value
+                                                                )
+                              )
+        self.assertEqual(len(resp), 2)
+
+    def test_indirect_or_unspecified_mechanism(self):
+        # P14778 (IL1R1)<-root MF-causally upstream of, positive effect->MF->P51617 (IRAK1)
+        # From SIGNOR-IL1R
+        stmt_file = "resources/test/SIGNOR-IL1R.tsv"
+        model = generate_model(stmt_file, "SIGNOR - IL1 Receptor")
+        resp = self.run_query(model, self.gen_causal_stmt_query(entity_a="P14778",
+                                                                mechanism="GO:0003674",
+                                                                entity_b="P51617",
+                                                                reg_relation=OntologyTerm.CAUSALLY_UPSTREAM_OF_POSITIVE_EFFECT.value
+                                                                )
+                              )
+        self.assertEqual(len(resp), 1)
+
     def test_intermediary_bp_patterns(self):
+        # Regulation by ubiquitination and degradation
+        # From SIGNOR-LBC
         stmt_file = "resources/test/SIGNOR-LBC.tsv"
         model = generate_model(stmt_file, "SIGNOR - Luminal Breast Cancer")
 
-        # Should only be one: Q00987 <-enabled_by- GO:0061630 -regulates-> GO:0043161 -regulates-> MF -enabled_by-> P04637
+        # Q00987 (MDM2)<-enabled_by- GO:0061630 -regulates-> GO:0043161 -regulates-> MF -enabled_by-> P04637 (TP53)
         resp = self.run_query(model, self.gen_intermediary_bp_stmt_query(entity_a="Q00987",
                                                                          mechanism="GO:0061630",
                                                                          intermediary_bp="GO:0043161",
                                                                          intermediary_relation=OntologyTerm.POSITIVELY_REGULATES.value,
                                                                          entity_b="P04637",
                                                                          reg_relation=OntologyTerm.NEGATIVELY_REGULATES.value
-                                                                        )
+                                                                         )
                               )
-        print(len(resp), "matches found")
+        self.assertEqual(len(resp), 1)
+
+        # Transcriptional regulation
+        # From SIGNOR-AC
+        stmt_file = "resources/test/SIGNOR-AC.tsv"
+        model = generate_model(stmt_file, "SIGNOR - Adipogenesis")
+
+        # P16220 (CREB1)<-enabled_by- GO:0140110 -positively_regulates-> GO:0009299 -positively_regulates-> MF -enabled_by-> P17676 (CEBPB)
+        resp = self.run_query(model, self.gen_intermediary_bp_stmt_query(entity_a="P16220",
+                                                                         mechanism="GO:0140110",
+                                                                         intermediary_bp="GO:0009299",
+                                                                         intermediary_relation=OntologyTerm.POSITIVELY_REGULATES.value,
+                                                                         entity_b="P17676",
+                                                                         reg_relation=OntologyTerm.POSITIVELY_REGULATES.value
+                                                                         )
+                              )
         self.assertEqual(len(resp), 1)
 
     def test_pathway_connection_filter_protein_binding(self):
@@ -161,9 +230,31 @@ class TestSignor2Gocam(unittest.TestCase):
 
     def test_small_molecule_patterns(self):
         stmt_file = "resources/test/SIGNOR-smallmol.tsv"
+        # CHEBI:16382 (iodide)-is_sm_mol_act->root MF->P07202 (TPO) (TC)
+
         model = generate_model(stmt_file, "SIGNOR - Small molecule test")
 
-        self.assertEqual(1, 1)
+        # Catalysis
+        # Q08828 (ADCY1)<-catalytic activity-has_output->CHEBI:17489 (3',5'-cyclic AMP)
+        # From SIGNOR-Hedgehog
+        resp = self.run_query(model, self.gen_participant_stmt_query(entity_a="Q08828",
+                                                                     mechanism="GO:0003824",
+                                                                     entity_b="CHEBI:17489",
+                                                                     participant_relation=OntologyTerm.HAS_OUTPUT.value
+                                                                     )
+                              )
+        self.assertEqual(len(resp), 1)
+
+        # P60484 (PTEN)<-catalytic activity-has_input->CHEBI:16618 (1-phosphatidyl-1D-myo-inositol 3,4,5-trisphosphate)
+        # From SIGNOR-GBM
+        resp = self.run_query(model, self.gen_participant_stmt_query(entity_a="P60484",
+                                                                     mechanism="GO:0003824",
+                                                                     entity_b="CHEBI:16618",
+                                                                     participant_relation=OntologyTerm.HAS_INPUT.value
+                                                                     )
+                              )
+        # There will be two of these
+        self.assertEqual(len(resp), 2)
 
 
 if __name__ == '__main__':
